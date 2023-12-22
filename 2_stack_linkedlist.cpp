6cf3e9f3cf57b3509667c7c14ca738efe634f88b
mohits44 <101943239+mohits44@users.noreply.github.com>
2023-12-22 13:38:37 +0530

    Update 2_stack_linkedlist.cpp

--- 2_stack_linkedlist.cpp
+++ 2_stack_linkedlist.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+void push(int x)
+{
+    Node *t = new Node;
+    if(t==NULL)
+    {
+        cout<<"stack overflow";
+    }
+    else
+    {
+        t->data=x;
+        t->next=top;
+        top=t;
+    }
+}
+int pop()
+{
+    struct node *t;
+    int x=-1;
+    if(top==NULL)
+    
+}
+int main() {
+    // Write C++ code here
+    
 
+    return 0;
+}
